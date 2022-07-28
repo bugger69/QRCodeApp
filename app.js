18922d243ac1c65d34b37ff49425347038893185
--global <--global>
2022-07-28 18:18:56 +0530

    finally stable

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
-import qrcode from "qrcodejs"
-
+//To generate QR Codes
 const form = document.querySelector('#choices');
 const dropdown = document.querySelector('#options');
 const linkForm = document.querySelector('.input-div .link-form');
 const inputField = document.querySelector('.input-div .link-form .input');
 const linkButton = document.querySelector('.link-submit');
 const qrCodeDisp = document.querySelector('#qrcodedisp');
-let nLinkboxes = 0;
 
-form.addEventListener('submit', (e) => {
+linkForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    if(dropdown.value === "generate" && nLinkboxes === 0) {
-        addLinkbox();
-        nLinkboxes++;
-    } else {
-        //
-    }
+    CreateQR(inputField.value);
 });
 
-function addLinkbox() {
-    const input = document.createElement('input');
-    const label = document.createElement('label');
-    label.textContent = 'Enter the Link: ';
-    input.id = "link-in";
-    label.htmlFor = "link-in";
-    inputField.append(label);
-    inputField.append(input);
-    updateButton(linkButton, input);
-}
-
-function updateButton(Button, inp) {
-    Button.textContent = "Convert";
-    Button.addEventListener('click', (e) => {
-        e.preventDefault();
-        let link = inp.value;
-        CreateQR(link);
-    });
-    Button.addEventListener('keydown', (e) => {
-        e.preventDefault();
-        if(e.keyCode == 13) {
-            let link = inp.value;
-            CreateQR(link);
-        }
-    })
-}
 
 function CreateQR(link) {
     qrCodeDisp.innerHTML = "";
@@ -56,3 +23,62 @@ function CreateQR(link) {
         correctLevel : QRCode.CorrectLevel.H
     });
 }
+
+//to scan qr codes(shit ton of work needed)
+(function() {
+    let canvas = document.getElementById("canvasElement");
+    let context = canvas.getContext('2d');
+    let video = document.getElementById("videoElement");
+    let vendorURL = window.URL || window.webkitURL;
+
+    navigator.mediaDevices.getUserMedia({
+        video: true, 
+        audio: false
+    })
+    .then((stream) => {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); 
+        video.play();
+        requestAnimationFrame(tick);
+    })
+    .catch(err => console.log(err))
+
+    video.addEventListener('play', function () {
+        draw(this, context, 400, 300);
+    }, false);
+
+    function draw(vid, cont, width, height) {
+        cont.drawImage(vid, 0, 0, width, height);
+        setTimeout(draw, 10, vid, cont, width, height);
+    }
+
+    function tick() {
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            let imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
+            let code = jsQR(imageData.data, imageData.width, imageData.height, {
+              inversionAttempts: "dontInvert",
+            });
+            if (code) {
+                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+                console.log(code.data);
+                video.src = "";//almost there
+                canvas.hidden = true;
+            } else {
+                console.log("Not Working!!!!");
+            }
+        }
+        requestAnimationFrame(tick);
+    }
+
+    function drawLine(begin, end, color) {
+        context.beginPath();
+        context.moveTo(begin.x, begin.y);
+        context.lineTo(end.x, end.y);
+        context.lineWidth = 4;
+        context.strokeStyle = color;
+        context.stroke();
+    }
+})();
